629061b6dd0b237a3068d11d450d18f27722999d
Dael777 <dael85@gmail.com>
2018-08-01 14:57:30 +0300

    Gallery added

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -16,15 +16,22 @@ export default class PhoneViewer extends Component {
     this._render();
 
     super.show();
+
+    this.on('click', '.phone-thumbs img', (event) => {
+    	let galleryImage = event.delegateTarget,
+    		galleryImageLink = galleryImage.getAttribute('src'),
+    		large_image = document.querySelector('#large_image');
+    		
+    	large_image.setAttribute('src', galleryImageLink);
+    })
   }
 
   _render() {
     this._element.innerHTML = `
-      <img class="phone" src="${this._phone.images[0]}">
+      <img id="large_image" class="phone" src="${this._phone.images[0]}">
 
       <button>Back</button>
       <button>Add to basket</button>
-  
   
       <h1>${this._phone.id}</h1>
   
