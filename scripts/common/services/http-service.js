dd57e8e86dbff93c08038a743163f916dd0d03d6
Dmitriy Lomakin <lomakin.d@gmail.com>
2018-08-06 22:11:03 +0300

    Changes API URL so the project works on the GitHub pages

--- scripts/common/services/http-service.js
+++ scripts/common/services/http-service.js
@@ -1,5 +1,5 @@
-// const API_URL = 'https://mgrinko.github.io/js-20180614/api/';
-const API_URL = 'http://localhost:3000/api/';
+const API_URL = 'https://for93t.github.io/js-20180614-by-for93t/api/';
+// const API_URL = 'http://localhost:3000/api/';
 
 const HttpService = {
   sendRequest(url, successCallback, errorCallback) {
