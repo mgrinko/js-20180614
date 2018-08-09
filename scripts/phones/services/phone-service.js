e5a5ce69c008f05613a1a1d5ecfcc49a30ecdecd
mgrinko <glukym@gmail.com>
2018-08-09 18:07:07 +0200

    promise was added

--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -8,8 +8,100 @@ const PhoneService = {
   },
 
   get(phoneId, callback) {
-    HttpService.sendRequest(`phones/${phoneId}.json`, callback)
+    // HttpService.sendRequest(`phones/${phoneId}.json`, callback)
+    let promise = this._sendRequest(`phones/${phoneId}.json`);
+    promise.then(callback)
   },
+
+  _sendRequest(url) {
+    let promise = {
+      _successCallbacks: [],
+
+      then(successCallback) {
+        this._successCallbacks.push(successCallback);
+      },
+
+      _resolve(data) {
+        this._successCallbacks.forEach(callback => {
+          callback(data)
+        });
+      }
+    };
+
+    HttpService.sendRequest(url, (data) => {
+      promise._resolve(data)
+    })
+
+    return promise
+  }
 };
 
 export default PhoneService;
+
+
+
+
+
+
+
+
+
+// function getAcademy (subdomain, successCallback, errorCallback) {}
+// function getUser (token, successCallback) {}
+// function getCourse (courseId, successCallback) {}
+// function getUserCourse (userId, courseId, successCallback) {}
+//
+//
+// getUser('qweqweqweqwe', (user) => {
+//   if (!academy) {
+//     return
+//   }
+//
+// }, () => {})
+//
+//
+// getAcademy('academy', (academy) => {
+//   if (!user) {
+//     return
+//   }
+//
+//   if (!user.hasAccessTo(academy) ) {
+//     // redirect to error page
+//   }
+//
+//   getCourse(123, (course) => {
+//     getUserCourse(user.id, course.id, (progress) => {
+//
+//       getTopic(progress.lastTopicId, (topic) => {
+//         renderTopic(topic)
+//       }, () => {})
+//     }, () => {})
+//   }, () => {})
+// }, () => {})
+//
+// getAcademy()
+//   .then(academy => {
+//
+//     return getUser()
+//   })
+//   .then(user => {
+//
+//     return getCourse()
+//   })
+//   .then(course => {
+//
+//
+//     return getUserCourse()
+//   })
+//   .catch(() => {})
+//
+//
+// if (academy) {
+//   return
+// }
+//
+// let user = getuser()
+// let user = getuser()
+// let user = getuser()
+// let user = getuser()
+// let user = getuser()
