a3a36a867843abe5e1576f360029bf2bd6c14bd2
IIIemo <deniszhibenkov@gmail.com>
2018-08-12 10:36:37 +0300

    Add promise

--- scripts/phones/services/phone-service.js
+++ scripts/phones/services/phone-service.js
@@ -8,8 +8,32 @@ const PhoneService = {
     },
 
     get(phoneId, callback) {
-        HttpService.sendRequest(`phones/${phoneId}.json`, callback);
+        // HttpService.sendRequest(`phones/${phoneId}.json`, callback);
+        let promise = this._sendRequest(`phones/${phoneId}.json`);
+        promise.then(callback);
     },
+
+    _sendRequest(url) {
+        let promise = {
+            _successCallBacks: [],
+
+            then(successCallback) {
+                this._successCallBacks.push(successCallback);
+            },
+
+            _resolve(data) {
+                this._successCallBacks.forEach(callback => {
+                    callback(data);
+                })
+            },
+        };
+
+        HttpService.sendRequest(url, data => {
+            promise._resolve(data);
+        });
+
+        return promise;
+    }
 };
 
 export default PhoneService;
